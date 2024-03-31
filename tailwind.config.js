674556f50c0e3da482229623f25d65746bf76679
Filip <filip_bos.feit@hotmail.com>
2024-03-31 14:18:41 +0200

    Installed Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    colors: {
+      "light-blue": "#E4FAFF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
